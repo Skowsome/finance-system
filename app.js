96dc27154ad93440d97a27d87c35be6c310730ce
Skowsome <skowsome@gmail.com>
2023-10-19 17:20:19 +0500

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+var currentTab = 0;
+
+
+
+function changeTab(index){
+    if (index===0) {
+        document.getElementById('0').style.display = 'block';
+        document.getElementById('1').style.display = 'none';
+        document.getElementById('2').style.display = 'none';
+        document.getElementById('3').style.display = 'none';
+    } else if(index===1){
+        document.getElementById('0').style.display = 'block';
+        document.getElementById('1').style.display = 'none';
+        document.getElementById('2').style.display = 'none';
+        document.getElementById('3').style.display = 'none';
+    } 
+    else if(index===2){
+        document.getElementById('0').style.display = 'block';
+        document.getElementById('1').style.display = 'none';
+        document.getElementById('2').style.display = 'none';
+        document.getElementById('3').style.display = 'none';
+    } 
+}
+
